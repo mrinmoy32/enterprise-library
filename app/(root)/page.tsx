2aedf83cca61b8adf21be729032d3aa7d7f5761d
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-07-06 23:07:34 +0530

    Add bookoverview and booklist comps

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button"
+import BookOverview from "@/components/BookOverview"
+import BookList from "@/components/BookList"
+
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <>
+    <BookOverview />
+    <BookList />
+    </>
   )
 }
