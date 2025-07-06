8f8e865bc3f48844c4ae4aa234460783344fa3d6
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-07-06 23:06:12 +0530

    add header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from 'react'
+import Header from '@/components/Header'
 
 const layout = ({children}: {children: ReactNode}) => {
   return (
     <main className='root-container'>
         <div className='mx-auto max-w-7xl'>
-            Header
+            <Header />
             <div className='mt-20 pb-20'>
                 {children}
             </div>
