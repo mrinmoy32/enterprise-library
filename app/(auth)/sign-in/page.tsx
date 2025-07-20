72fff79dcef9dec5fb4e6bdfbb804d48f49b4105
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-07-20 20:30:55 +0530

    sign in form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,15 @@
+import AuthForm from '@/components/AuthForm'
+import { signInSchema } from '@/lib/validations'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>sign in</div>
+    <AuthForm
+    type="SIGN_IN"
+    schema={signInSchema}
+    default={{ email: '', password: '' }}
+    onSubmit={() => {}}
+    />
   )
 }
 
