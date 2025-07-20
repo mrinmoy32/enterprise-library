d309eeb8b6b66223d82740400e8b28e2f0e67868
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-07-20 20:31:13 +0530

    sign up form

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import AuthForm from '@/components/AuthForm';
+import {  signUpSchema } from '@/lib/validations';
 
 const page = () => {
   return (
-    <div>sign up</div>
+    <AuthForm
+    type="SIGN_Up"
+    schema={signUpSchema}
+    default={{fullName: '', email: '', universityId: 0, universityCard: '', password: ''}}
+    onSubmit={() => {}}
+    />
   )
 }
 
