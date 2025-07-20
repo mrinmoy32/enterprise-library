2e01cfab12b234b6d38be05e676087e8ccd9ce2b
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-07-20 13:53:32 +0530

    auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,21 @@
-import { ReactNode } from 'react'
+import { ReactNode } from "react";
+import Image from "next/image";
 
-const layout = ({children}: {children: ReactNode}) => {
+const layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>{children}</div>
-  )
-}
+    <main className="auth-container">
+      <section className="auth-form">
+        <div className="auth-box">
+          <div className="flex flex-row gap-3">
+            <Image src="/icons/logo.svg" alt="logo" width={37} height={37} />
+            <h1 className="text-2xl font-semibold text-white">BookWise</h1>
+          </div>
+          <div>{children}</div>
+        </div>
+      </section>
+      <section className="auth-ill"></section>
+    </main>
+  );
+};
 
-export default layout
+export default layout;
