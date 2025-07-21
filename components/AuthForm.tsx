7424a2fd293da863d7b4cab15e5c63777b185dd8
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-07-21 12:36:10 +0530

    render all fields using map and additions texts

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -2,13 +2,9 @@
 
 import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  FieldValues,
-  useForm,
-  UseFormReturn,
-} from "react-hook-form";
+import { FieldValues, Path, useForm, UseFormReturn } from "react-hook-form";
 import { z, ZodType } from "zod";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -17,8 +13,10 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { FIELD_NAMES } from "@/constants";
 
 interface Props<T extends FieldValues> {
   schema: ZodType<T>;
@@ -33,6 +31,8 @@ const AuthForm = <T extends FieldValues>({
   defaultValues,
   onSubmit,
 }: Props<T>) => {
+  const isSignIn = type === "SIGN_IN";
+
   // 1. Define your form.
   const form: UseFormReturn<T> = useForm({
     resolver: zodResolver(schema),
@@ -41,27 +41,52 @@ const AuthForm = <T extends FieldValues>({
 
   const hanldeSubmit: SubmitHandler<T> = async (data) => {};
 
-  return <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>;
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-semibold text-white">
+        {isSignIn ? "Welcome Back to BookWise" : "Create your library account"}
+      </h1>
+      <p className="text-light-100">
+        {isSignIn
+          ? "Access the vast collection of resources, and stay updated"
+          : "Please complete all fields and upload a valid university ID to gain access to the library"}
+      </p>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit()} className="space-y-8 w-full">
+          {Object.keys(defaultValues).map((field) => (
+            <FormField
+            key={field}
+              control={form.control}
+              name={field as Path<T>}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="capitalize ">{FIELD_NAMES[field.name]}</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+      <p className="text-center text-base font-medium">
+        {isSignIn ? "New to BookWise? " : "Already have an account? "}
+        <Link
+          href={isSignIn ? "/sign-up" : "/sign-in"}
+          className="font-bold text-primary"
+        >
+          {isSignIn ? "Create an account" : "Sign In"}
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default AuthForm;
