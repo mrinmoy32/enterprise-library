be4bfec2e51d62b6b2cb518ec019f57b38228acc
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-08-12 19:57:39 +0530

    Adds image key pachange and env secrets

---
+++ lib/config.ts
@@ -0,0 +1,11 @@
+const config = {
+    env: {
+        imageKit: {
+            publickey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
+            urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
+            privatekey: process.env.IMAGEKIT_PRIVATE_KEY,
+        }
+    }
+}
+
+export default config;
