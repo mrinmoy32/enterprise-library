babb12244d2625665a057af7e2f993d8dbdb6ad2
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-08-16 11:45:55 +0530

    route

--- lib/config.ts
+++ lib/config.ts
@@ -1,10 +1,10 @@
-const config = {
+ export const config = {
     env: {
         apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
         imageKit: {
-            publickey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+            publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
             urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
-            privatekey: process.env.IMAGEKIT_PRIVATE_KEY!,
+            privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
         }
     }
 }
