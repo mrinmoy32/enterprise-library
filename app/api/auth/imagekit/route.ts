a952898b7825f9e53bf8a410d136bb941251169a
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-08-18 12:24:22 +0530

    getUploadAuthParams

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -1,15 +1,34 @@
-import ImageKit from "imagekit";
+// import ImageKit from "imagekit";
 import { config } from "@/lib/config";
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
+// File: app/api/upload-auth/route.ts
+import { getUploadAuthParams } from "@imagekit/next/server"
 
 const {env: {imageKit: {publicKey, privateKey, urlEndpoint}}} = config
 
-const imagekit = new ImageKit({
-    publicKey,
-    privateKey,
-    urlEndpoint,
-});
+// const imagekit = new ImageKit({
+//     publicKey,
+//     privateKey,
+//     urlEndpoint,
+// });
+
+// export async function GET() {
+//     return NextResponse.json(imagekit.getAuthenticationParameters)
+// }
+
+
 
 export async function GET() {
-    return NextResponse.json(imagekit.getAuthenticationParameters)
+    // Your application logic to authenticate the user
+    // For example, you can check if the user is logged in or has the necessary permissions
+    // If the user is not authenticated, you can return an error response
+
+    const { token, expire, signature } = getUploadAuthParams({
+        privateKey: privateKey as string, // Never expose this on client side
+        publicKey: publicKey as string,
+        // expire: 30 * 60, // Optional, controls the expiry time of the token in seconds, maximum 1 hour in the future
+        // token: "random-token", // Optional, a unique token for request
+    })
+
+    return Response.json({ token, expire, signature, publicKey: publicKey })
 }
